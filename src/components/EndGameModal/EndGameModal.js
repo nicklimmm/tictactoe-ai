7ea245b27a711bd59d13171c91da6dd98a7a1bba
Nicky Lim <nickylim.p@gmail.com>
2021-04-29 00:01:58 +0800

    added button to end modal

--- src/components/EndGameModal/EndGameModal.js
+++ src/components/EndGameModal/EndGameModal.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Modal, Paper } from "@material-ui/core"
+import { Modal, Paper, Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { GameContext } from "../../providers/GameProvider"
 import { ModeContext } from "../../App"
@@ -20,7 +20,7 @@ const useStyles = makeStyles({
 const EndModal = () => {
   const classes = useStyles()
   const { mode, setMode } = useContext(ModeContext)
-  const { grid, clearGrid } = useContext(GameContext)
+  const { grid } = useContext(GameContext)
 
   const isDone = () => {
     return isFull(grid) || getWinner(grid) !== ""
@@ -30,26 +30,34 @@ const EndModal = () => {
     let winner = getWinner(grid)
 
     if (winner === "") return "Tied"
-    // reversed
+    // Human vs Human message
     else if (mode === "HH") {
       return winner === "X" ? "Player 1 Wins!" : "Player 2 Wins!"
-    } else {
+    }
+
+    // Human vs Bot message
+    else {
       return winner === "X" ? "You Win!" : "You Lose!"
     }
   }
 
   const handleClose = () => {
     setMode("")
-    clearGrid()
   }
 
   return (
     <Modal open={isDone()} onClose={handleClose} className={classes.modal}>
       <Paper className={classes.paper}>
         <h2>{displayWinner()}</h2>
+
+        {/* Only display when playing with bots */}
         {mode !== "HH" && getWinner(grid) === "O" && (
           <p>{"Try again next time :)"}</p>
         )}
+
+        <Button color="primary" variant="contained" onClick={handleClose}>
+          Return
+        </Button>
       </Paper>
     </Modal>
   )
